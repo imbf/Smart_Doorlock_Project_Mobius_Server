1078c3aa1029523cb9443e667a79efd3b6e73984
imbf <qowhdwlsdk@gmail.com>
2019-10-05 01:51:33 +0900

    test

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ var doorlockdb = mysql.createConnection({
     user: 'root',
     password: '1111',
     database: 'doorlockdb',
-    timezone:'Asia/Seoul'
+    timezone:'UTC+9'
 })
 
 //JongJin Defined
