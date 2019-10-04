3055d8891193b17b350a6060d459e1b259af14c9
imbf <qowhdwlsdk@gmail.com>
2019-10-05 02:01:22 +0900

    Test

--- app.js
+++ app.js
@@ -1257,7 +1257,7 @@ function lookup_create(request, response) {
                                 Mrequest(Servooptions);
                             }    
                         });
-                        doorlockdb.query('select disposablepassword from smsservice where activetime<=now() and unactivetime>=now() and opennumber=0',function(error,result,fields){
+                        doorlockdb.query(`select disposablepassword from smsservice where activetime<=${date} and unactivetime>=${date} and opennumber=0`,function(error,result,fields){
                             console.log("일회용 비밀번호 : " ,result)
                             /*
                             if(result[0].disposablepassword === body_Obj.cin.con){
