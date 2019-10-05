0a6678bc1f87b113e48695143bc4c9bf341bcf41
imbf <qowhdwlsdk@gmail.com>
2019-10-05 11:26:45 +0900

    test

--- app.js
+++ app.js
@@ -1260,8 +1260,8 @@ function lookup_create(request, response) {
                         doorlockdb.query(`select disposablepassword from smsservice where activetime<='${date}' and unactivetime>='${date}' and opennumber=0`,function(error,result,fields){
                             console.log(result);
                             if(result[0]!=undefined){
-                                for(var index in result[0]){
-                                    console.log(result[0][index]);
+                                for(var index in result){
+                                    console.log(result[index]);
                                 }
                                 
                             }
